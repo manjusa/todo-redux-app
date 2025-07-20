f10b31da781b7055b4e87c5bfa7c1633318cb821
manjusa <thizismanju@mail.com>
2025-07-20 19:38:20 +1000

    remove vitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,8 +12,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
