7ebf4015326e8adde2f76d959b42564fc6b14169
manjusa <thizismanju@mail.com>
2025-07-20 20:37:58 +1000

    removed unwatned components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Todo } from "./features/todos/Todo";
+// import { Todo } from "./features/todos/Todo";
 import { TodoList } from "./features/todos/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      {/* <Todo /> */}
       <TodoList />{" "}
     </div>
   );
